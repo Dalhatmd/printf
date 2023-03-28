ecf121146ba74e40616c181c213554881bdba1e4
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-28 16:08:46 -0700

    bugFix

--- _print_int.c
+++ _print_int.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _print_int - prints an integer as a string.
@@ -11,34 +12,39 @@ int _print_int(int num)
 	int i, counter = 0, index = 0, temp_num = num, num_digits = 0;
 	char *str;
 
-	/* count the number of digits in the number*/
-	do {
-		num_digits++;
-		temp_num /= 10;
-	} while (temp_num > 0);
-
-	/* allocate a buffer to hold the string representation of the number*/
-	str = malloc(sizeof(char) * num_digits + 1);
-	if (str == NULL)
-		return (1);
-
-	/* handle negative numbers*/
-	if (num < 0)
+	if (num >= INT_MIN && num <= INT_MAX)
 	{
-		counter += _putchar('-');
-		num = -num;
-	}
+		/* count the number of digits in the number*/
+		do {
+			num_digits++;
+			temp_num /= 10;
+		} while (temp_num > 0);
 
-	/* convert the number to a string*/
-	do {
-		str[index++] = num % 10 + '0';
-		num /= 10;
-	} while (num > 0);
+		/* allocate a buffer to hold the string representation of the number*/
+		str = malloc(sizeof(char) * num_digits + 1);
+		if (str == NULL)
+			return (1);
 
-	/* write the string to stdout in reverse order*/
-	for (i = index - 1; i >= 0; i--)
-	{
-		counter += write(1, &str[i], 1);
+		/* handle negative numbers*/
+		if (num < 0)
+		{
+			counter += _putchar('-');
+			num = -num;
+		}
+
+		/* convert the number to a string*/
+		do {
+			str[index++] = num % 10 + '0';
+			num /= 10;
+		} while (num > 0);
+
+		/* write the string to stdout in reverse order*/
+		for (i = index - 1; i >= 0; i--)
+		{
+			counter += write(1, &str[i], 1);
+		}
+		return (counter);
 	}
-	return (counter);
+	else
+		return (0);
 }

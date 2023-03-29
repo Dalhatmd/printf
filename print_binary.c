06b3d35dd98e345b253e665abcb7cf11e616f4f6
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-29 15:16:56 -0700

    betty checks

--- print_binary.c
+++ print_binary.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
  * print_binary - prints a number in binary
- *
  * @num: number to be printed
+ *
  * Return: count of digits
  */
 int print_binary(unsigned int num)
 {
-    int count;
+	int count;
 
-    count = 0;
-    if (num / 2 != 0)
-        count += print_binary(num / 2);
-
-    _putchar(num % 2 == 0 ? '0' : '1');
-    count++;
-
-    return (count);
+	count = 0;
+	if (num / 2 != 0)
+		count += print_binary(num / 2);
+	_putchar(num % 2 == 0 ? '0' : '1');
+	count++;
+	return (count);
 }
